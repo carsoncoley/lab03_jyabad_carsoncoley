b84df3599f74874d90359801d95f4233bc674149
carsoncoley <43832363+carsoncoley@users.noreply.github.com>
2018-10-30 15:43:15 -0700

    Create myProg01.cpp

---
+++ myProg01.cpp
@@ -0,0 +1,16 @@
+// myProg01.cpp
+// Author: Carson Coley and Niko Abad
+#include <iostream>
+using namespace std;
+ 
+int main() {
+     // Simple for loop that counts down from n to 1
+ 
+     int n=5;
+ 
+     for (int i=n; i>0; i--) {
+          cout << i << " ";
+     }
+     cout<<endl;
+     return 0;
+}
